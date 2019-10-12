21b93947c568488082009d64604bcbd4a1d41f7b
i-lab staff <i-labstaff@MXTech81831Macbook.local>
2019-10-12 10:57:34 -0700

    Finished JukeBox
    11 animal tokens

--- src/_03_jukebox/JukeboxRunner.java
+++ src/_03_jukebox/JukeboxRunner.java
@@ -9,31 +9,66 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class JukeboxRunner implements ActionListener {
+
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+
+	JButton button1 = new JButton();
+	JButton button2 = new JButton();
+	JButton button3 = new JButton();
+
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Jukebox());
 
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
+		JukeboxRunner test = new JukeboxRunner();
 
-		JButton button1 = new JButton();
-		JButton button2 = new JButton();
-		JButton button3 = new JButton();
+		test.buildUI();
 
-		button1.addActionListener(null);
-		button2.addActionListener(null);
-		button3.addActionListener(null);
+	}
 
-	Song test1 = new Song("AllStar.mp3");
+	void buildUI() {
 
-	test1.loadFile();
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		button3.addActionListener(this);
 
-	test1.play();
+		frame.add(panel);
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
 
+		button1.setText("All Star");
+		button2.setText("Another One Bites the Dust");
+		button3.setText("Bohemian Rhapsody");
 
+		frame.setVisible(true);
+		frame.pack();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+
+		Song test1 = new Song("AllStar.mp3");
+		Song test2 = new Song("Dust.mp3");
+		Song test3 = new Song("QBR.mp3");
+
+		if (e.getSource() == button1) {
+			System.out.println("button 1 was pressed");
+			test1.loadFile();
+			test1.play();
+
+		} else if (e.getSource() == button2) {
+			System.out.println("button 2 was pressed");
+			test2.loadFile();
+			test2.play();
+
+		} else if (e.getSource() == button3) {
+			System.out.println("button 3 was pressed");
+			test3.loadFile();
+			test3.play();
+
+		} else {
+			System.out.println("Bruh this doesn't work.");
+		}
 
 	}
 }
