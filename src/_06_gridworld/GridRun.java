1bad63e3a91e24e7d6b7db0b93b435d2d2827b7b
League <league@WorthyMini.local>
2019-11-16 11:27:13 -0800

    Finished GridWorld and Finished CowTimer
    14 animal tokens

--- src/_06_gridworld/GridRun.java
+++ src/_06_gridworld/GridRun.java
@@ -1,16 +1,29 @@
 package _06_gridworld;
 
+import java.awt.Color;
+
 import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
 import info.gridworld.grid.Location;
 import info.gridworld.world.World;
 
 public class GridRun {
 	public static void main(String[] args) {
-      //Hans stop looking at my screen!!!
 		World world = new World();
 		world.show();
 		Bug bug = new Bug();
 		Bug bug2 = new Bug();
+		
+		int y = 0;
+		
+		Flower flower1 = new Flower();
+		Flower flower2 = new Flower();
+		
+		bug.setColor(Color.blue);
+		
+		for(int i = 0; i < 2; i++) {
+			bug.turn();
+		}
 		
 		Location bruh = new Location(1,1);
 		Location bruh2 = new Location(2,1);
@@ -23,6 +36,25 @@ public class GridRun {
 //		Location bruh = new Location(1,1);
 		world.add(bruh, bug);
 		world.add(bruh4, bug2);
+		world.add(bruh2, flower1);
+		world.add(bruh3, flower2);
+		
+		for(int i = 0; i < 11; i++) {
+			if(i == 10) {
+				i = 0;
+				y+=1;
+			}
+			if(y == 10) {
+				y = 0;
+			}
+			
+			Location bruh9000 = new Location(i , y);
+			Flower flower9000 = new Flower(Color.BLACK);
+			world.add(bruh9000, flower9000);
+			
+			
+		}
+		
 	}
 	
 }
--- src/_07_cow_timer/CowTimer.java
+++ src/_07_cow_timer/CowTimer.java
@@ -9,13 +9,16 @@ import java.io.IOException;
 import javax.swing.JApplet;
 
 public class CowTimer {
+	private int minutes;
 
 	/* 1. Make a constructor for the CowTimer class that initializes the minutes variable */
+	public CowTimer(int minutes){
+		this.minutes = minutes;
+	}
 	
 	/* 4. Complete the main method of the CowTimerRunner class */
 
-	private int minutes;
-
+	
 	public void setTime(int minutes) {
 		this.minutes = minutes;
 		System.out.println("Cow time set to " + minutes + " minutes.");
@@ -25,12 +28,22 @@ public class CowTimer {
 		/*
 		 * 2. Count down the minutes, print the current minute then sleep for the number of minutes
 		 * using Thread.sleep(int milliseconds). 
+		 * 
 		 */
+		
+			while(minutes>0) {
+				System.out.println(minutes);
+				minutes--;
+				Thread.sleep(60000);
+			}
+			if(minutes == 0) {
+				playSound("moo.wav");
+			}
 
 		/*
 		 * 3. When the timer is finished, use the playSound method to play a moo sound.
 		 * You can use the .wav file in the default package, or you can download one
-		 * from freesound.org, then drag it intothe default package.
+		 * from freesound.org, then drag it into the default package.
 		 */
 
 	}
